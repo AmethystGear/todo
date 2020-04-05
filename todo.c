a0c027b3ef3541d4c6edb7bf3f3fabc03b47dc90
AmethystGear <shivenbhatt@outlook.com>
2020-04-05 13:30:05 -0700

    improved date system so that it could handle years.

--- todo.c
+++ todo.c
@@ -98,39 +98,18 @@ int num_lines_in_file(char *fileName) {
     return numLines;
 }
 
-bool parse_date(char *date, int *month, int *day) {
-    int len = strlen(date);
-    int slash = -1;
-    for(int i = 0; i < len; i++) {
-        if(date[i] == '/') {
-            slash = i;
-            break;
-        }
-    }
-    if(slash == -1) {
-        return false;
-    } else {
-        char monthBuff[2];
-        char dayBuff[2];
-        memcpy(monthBuff, &date[0], slash);
-        memcpy(dayBuff, &date[slash + 1], len - slash);
-
-        int possibleMonth = atoi(monthBuff);
-        int possibleDay = atoi(dayBuff);
-        if(possibleDay > 31 || possibleDay < 1) {
-            return false;
-        }
-        if(possibleMonth > 12 || possibleMonth < 1) {
-            return false;
-        }
-        *month = possibleMonth;
-        *day = possibleDay;
-        return true;
-    }
-}
+struct Date {
+    int month;
+    int day;
+    int year;
+};
 
 bool is_leap_year(int year) {
     return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
+
+int days_in_year(int year) {
+    return is_leap_year(year) ? 366 : 355;
 }
 
 int days_in_month(int month, int year) {
@@ -140,45 +119,169 @@ int days_in_month(int month, int year) {
         return 28;
     } else if(month == 1 || month == 3 || month == 7 || month == 8 || month == 10 || month == 12) {
         return 31;
-    } else {
+    } else if(month == 2 || month == 4 || month == 5 || month == 6 || month == 9 || month == 11) {
         return 30;
+    } else {
+        return -1;
     }
 }
 
-int days_till_date(int month, int day) {
+bool is_valid_date(struct Date *date) {
+    if(date->month > 12 || date->month < 1) {
+        return false;
+    }
+    if(date->year < 0) {
+        return false;
+    }
+    int days = days_in_month(date->month, date->year);
+    if(date->day > days || date->day < 1) {
+        return false;
+    }
+    return true;
+}
+
+int compare_dates(struct Date *a, struct Date *b) {
+    if(a->day == b->day && a->month == b->month && a->year == b->year) {
+        return 0;
+    } else if((a->year < b->year) || (a->year == b->year && a->month < b->month) || (a->year == b->year && a->month == b->month && a->day < b->day)) {
+        return -1;
+    } else {
+        return 1;
+    }
+}
+
+bool parse_date(char *str, struct Date *date) {
+    int len = strlen(str);
+    int firstSlash = -1;
+    int secondSlash = -1;
+    for(int i = 0; i < len; i++) {
+        if(str[i] == '/' && firstSlash == -1) {
+            firstSlash = i;
+        } else if (str[i] == '/') {
+            secondSlash = i;
+            break;
+        }
+    }
+    
+    if(firstSlash == -1) {
+        printf("A");
+        return false;
+    } else {
+        char monthBuff[3];
+        char dayBuff[3];
+        char yearBuff[5];
+
+        if(firstSlash > 2) {
+            printf("B");
+            return false;
+        }
+        memcpy(monthBuff, &str[0], firstSlash);
+        monthBuff[2] = '\0';
+
+        if(secondSlash != -1) {
+            if(secondSlash - firstSlash - 1 > 2) {
+                return false;
+            }
+            memcpy(dayBuff, &str[firstSlash + 1], secondSlash - firstSlash - 1);            
+            if(len - secondSlash - 1 > 4) {
+                printf("D");
+                return false;
+            }
+            memcpy(yearBuff, &str[secondSlash + 1], len - secondSlash - 1);
+            yearBuff[4] = '\0';
+        } else {
+            if(len - firstSlash > 2) {
+                printf("E");
+                return false;
+            }
+            memcpy(dayBuff, &str[firstSlash + 1], len - firstSlash - 1);
+        }
+        dayBuff[2] = '\0';
+
+        int possibleMonth = -1;
+        int possibleDay = -1;
+        int possibleYear = -1;
+
+        possibleMonth = atoi(monthBuff);
+        possibleDay = atoi(dayBuff);
+        if(secondSlash != -1) {
+            possibleYear = atoi(yearBuff);
+        } else {
+            time_t s = time(NULL);
+            struct tm *currentTime;
+            currentTime = localtime(&s);
+            struct Date currDate = {
+                .day = currentTime->tm_mday, 
+                .month = currentTime->tm_mon + 1, 
+                .year = currentTime->tm_year + 1900
+            };
+            struct Date possibleDate = {
+                .day = possibleDay, 
+                .month = possibleMonth, 
+                .year = currentTime->tm_year + 1900
+            };
+            if(compare_dates(&possibleDate, &currDate) == -1) {
+                possibleYear = possibleDate.year + 1;
+            } else {
+                possibleYear = possibleDate.year;
+            }
+        }
+
+        date->day = possibleDay;
+        date->month = possibleMonth;
+        date->year = possibleYear;
+
+        if(!is_valid_date(date)) {
+            printf("F");
+            return false;
+        }
+        return true;
+    }
+}
+
+int days_between_dates(struct Date *start, struct Date *finish) {
+    if(compare_dates(start, finish) == 1) {
+        return -days_between_dates(finish, start);
+    }
+    if(finish->year > start->year) {
+        int totalDays = days_in_month(start->month, start->year) - start->day;
+        for(int i = start->month + 1; i <= 12; i++) {
+            totalDays += days_in_month(i, start->year);
+        }
+        for(int i = start->year + 1; i < finish->year; i++) {
+            totalDays += days_in_year(i);
+        }
+        for(int i = 1; i < finish->month; i++) {
+            totalDays += days_in_month(i, finish->year);
+        }
+        totalDays += finish->day;
+        return totalDays;
+    } else if (finish->month > start->month) {
+        int totalDays = days_in_month(start->month, start->year) - start->day;
+        for(int i = start->month + 1; i < finish->month; i++) {
+            totalDays += days_in_month(i, start->year);
+        }
+        totalDays += finish->day;
+        return totalDays;
+    } else {
+        return finish->day - start->day;
+    }
+}
+
+// calculate the days from now until the provided date.
+// if the date is earlier than the current date, returns negative days.
+int days_till_date(struct Date *date) {
     time_t s = time(NULL);
     struct tm *currentTime;
     currentTime = localtime(&s);
-    int currDay = currentTime->tm_mday;
-    int currMonth = currentTime->tm_mon + 1;
-    int currYear = currentTime->tm_year;
 
-    if(currMonth == month && currDay == day) {
-        return 0;
-    }
-    
-    if(month > currMonth) {
-        int daysLeftInMonth = days_in_month(currMonth, currYear) - currDay;
-        int totalDays = daysLeftInMonth;
-        for(int i = currMonth + 1; i < month; i++) {
-            totalDays += days_in_month(i, currYear);
-        }
-        totalDays += day;
-        return totalDays;
-    } else if (month == currMonth && day > currDay) {
-        return day - currDay;
-    } else {
-        int daysLeftInMonth = days_in_month(currMonth, currYear) - currDay;
-        int totalDays = daysLeftInMonth;
-        for(int i = currMonth + 1; i <= 12; i++) {
-            totalDays += days_in_month(i, currYear);
-        }
-        for(int i = 1; i < month; i++) {
-            totalDays += days_in_month(i, currYear + 1);
-        }
-        totalDays += day;
-        return totalDays;
-    }
+    struct Date currDate = {
+        .day = currentTime->tm_mday, 
+        .month = currentTime->tm_mon + 1, 
+        .year = currentTime->tm_year + 1900
+    };
+
+    return days_between_dates(&currDate, date);
 }
 
 int main(int argc, char *argv[]) {
@@ -213,38 +316,39 @@ int main(int argc, char *argv[]) {
             fprintf(fp, "%s\t\n", argv[3]);                    
             fclose(fp);
         } else {
+            struct Date date;
+            if(!parse_date(argv[4], &date)) {
+                printf("you didn't enter the date properly! You should enter it as mm/dd or mm/dd/yyyy\n");
+                exit(1);
+            }
+            char date_formatted[11];
+            sprintf(date_formatted, "%d/%d/%d", date.month, date.day, date.year);
+
             if (num_lines_in_file(TODO_FILENAME) == 0) {
                 FILE *fp = fopen(TODO_FILENAME, "a");
                 fprintf(fp, "%s\t", argv[3]);  
-                fprintf(fp, "%s\n", argv[4]);                        
+                fprintf(fp, "%s\n", date_formatted);                        
                 fclose(fp);
             } else {
-                int month = -1;
-                int day = -1;
-                if(!parse_date(argv[4], &month, &day)) {
-                    printf("you didn't enter the date properly! You should enter it as {month}/{day}.\n");
-                    exit(1);
-                }
                 FILE *fp = fopen(TODO_FILENAME, "r");
                 int c = 0;
                 int numLines = 0;
                 bool pastTab = false;
                 bool inserted = false;
                 int index = 0;
-                char date[6];
+                char str_date[11];
                 while ((c = getc(fp)) != EOF) {
                     if(c == '\t') {
                         pastTab = true;
                     }
                     else if(c == '\n') {
-                        date[index] = '\0';
-                        int lineMonth;
-                        int lineDay;
-                        if(strlen(date) != 0 && !parse_date(date, &lineMonth, &lineDay)){
+                        str_date[index] = '\0';
+                        struct Date lineDate;
+                        if(strlen(str_date) != 0 && !parse_date(str_date, &lineDate)){
                             printf("the provided file is in the wrong format!");
                             exit(1);
-                        } else if (strlen(date) == 0 || days_till_date(lineMonth, lineDay) > days_till_date(month, day)) {
-                            insert_task(numLines, argv[3], argv[4], TODO_FILENAME);
+                        } else if (strlen(str_date) == 0 || days_till_date(&lineDate) > days_till_date(&date)) {
+                            insert_task(numLines, argv[3], date_formatted, TODO_FILENAME);
                             inserted = true;
                             break;
                         }
@@ -253,15 +357,15 @@ int main(int argc, char *argv[]) {
                         index = 0;
                     }
                     else if(pastTab) {
-                        date[index] = c;
+                        str_date[index] = c;
                         index++;
                     }
                 }
                 fclose(fp);
                 if(!inserted) {
                     FILE *fp = fopen(TODO_FILENAME, "a");
-                    fprintf(fp, "%s\t", argv[3]);  
-                    fprintf(fp, "%s\n", argv[4]);                        
+                    fprintf(fp, "%s\t", argv[3]);
+                    fprintf(fp, "%s\n", date_formatted);                        
                     fclose(fp);
                 }
             }
